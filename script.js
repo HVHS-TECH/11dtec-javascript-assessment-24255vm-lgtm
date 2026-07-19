12974c983eb89b42e026cbd7f34775f8d341a06e
24255vm-lgtm <24255vm@hvhs.school.nz>
2026-07-19 12:20:36 +0000

    Jul 19, 2026, 12:20 PM

--- script.js
+++ script.js
@@ -14,25 +14,26 @@ let price = "$10";
 
 var output = "";
 
-for (var i = 0; i < menuItems.length; i++) {
-    output += `<div class="product">
+for (let i = 0; i < menuItems.length; i++) {
+    output += `
+        <div class="product">
+            <h3>${menuItems[i]}</h3>
+            <p>Price: ${price}</p>
 
             <label for="orders${i}">How many would you like?</label>
 
-            <input
-                type="number"
-                id="orders${i}"
-                min="-10"
-                value="0"
-                max="10"
-            >
+            <div class="orderControls">
+                <input
+                    type="number"
+                    id="orders${i}"
+                    min="0"
+                    max="10"
+                    value="0"
+                >
+                <button type="button">Submit</button>
+            </div>
         </div>
-            `;
+    `;
 }
-
-        <div class="orderControls">
-    <input type="number" id="orders${i}" min="0" value="0">
-    <button type="button">Submit</button>
-    </div>
 
 document.getElementById("menu").innerHTML = output;
