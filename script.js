aa8a4c655349aa5337ca0d3786ec6b26b8c69b2a
24255vm-lgtm <24255vm@hvhs.school.nz>
2026-07-19 11:06:56 +0000

    Jul 19, 2026, 11:06 AM

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ const output = "";
 for (var i = 0; i < menuItems.length; i++) {
     output += `<div class="product">
              <h3>${menuItems[i]}</h3> 
-             <p>Price: {price}</p>
-            <label for="qty${i}">How many would you like?</label>
-            <input type="number" id="qty${i}" min="0" value="0">
+             <p>Price: ${price}</p>
+            <label for="orders${i}">How many would you like?</label>
+            <input type="number" id="orders${i}" min="0" value="0">
         </div>
 }
 
