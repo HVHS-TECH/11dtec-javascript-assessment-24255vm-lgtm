0c73fa785aa572732f703b9cceb5e69a944d9d67
24255vm-lgtm <24255vm@hvhs.school.nz>
2026-07-19 11:10:52 +0000

    Jul 19, 2026, 11:10 AM

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ for (var i = 0; i < menuItems.length; i++) {
             <label for="orders${i}">How many would you like?</label>
             <input type="number" id="orders${i}" min="0" value="0">
         </div>
+        `;
 }
 
 document.getElementById("menu").innerHTML = output;
