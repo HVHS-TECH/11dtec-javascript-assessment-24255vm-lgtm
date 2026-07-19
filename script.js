05d9931e20a44c2bf5e82b9b4e80213226e82831
24255vm-lgtm <24255vm@hvhs.school.nz>
2026-07-19 11:32:54 +0000

    Jul 19, 2026, 11:32 AM

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ const menuItems = [
     "Mocha Guppy"
 ]
 
+const imgs = [
+    "guppy special.png",
+    "darkguppy.png",
+    "gupster.png",
+    "green guppy.png",
+    "vanilla guppy.png",
+    "mocha.png"
+];
+
 let price = "$10";
 
 const output = "";
@@ -17,6 +26,7 @@ for (var i = 0; i < menuItems.length; i++) {
     output += `<div class="product">
              <h3>${menuItems[i]}</h3> 
              <p>Price: ${price}</p>
+             <img src="${imgs[i]}" alt="${menuItems[i]}">
             <label for="orders${i}">How many would you like?</label>
             <input type="number" id="orders${i}" min="0" value="0">
         </div>
