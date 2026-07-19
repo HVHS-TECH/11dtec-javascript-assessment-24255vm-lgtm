d269ea1265c1ab458414d233f8f537fb8779f150
24255vm-lgtm <24255vm@hvhs.school.nz>
2026-07-19 13:17:57 +0000

    Jul 19, 2026, 1:17 PM

--- script.js
+++ script.js
@@ -26,6 +26,9 @@ var output = "";
 for (let i = 0; i < menuItems.length; i++) {
     output += `
         <div class="product">
+         <div class="product">
+            <img src="${images[i]}" alt="${menuItems[i]}" style="width:80%;">
+            
             <h3>${menuItems[i]}</h3>
             <p>Price: ${price}</p>
 
