a9c81efa3cfe3b53f255b54aa45856a9a552617c
24255vm-lgtm <24255vm@hvhs.school.nz>
2026-07-23 11:43:45 +0000

    Jul 23, 2026, 11:43 AM

--- all.js
+++ all.js
@@ -62,9 +62,10 @@ function blockInvalidKeys1(event1) {
 
 function lettersOnly(input) {
     input.value = input.value.replace(/[^a-zA-Z ]/g, "");
-    alert("Please enter a valid name.(no symbols or numbers)")
 }
-
+           {
+alert("Please enter a valid name.(no symbols or numbers)")
+}
 
 if (cartdiv && totaldiv) {
 
